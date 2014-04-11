6745180e52161b4e149bbc99b7bda2816340d7c5
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-04-11 16:15:34 -0600

    Added rewrite htaccess for index.php url

---
+++ application/controllers/index.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Index extends CI_Controller {
+
+   /**
+    */
+   public function index() {
+      parent::__construct();
+      $this->load->view('layout/bootstrap');
+   }
+}
---
+++ application/views/layout/bootstrap.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Easy Learn Tutorial: Free Programming Lessons</title>
+</head>
+
+<body>
+<h1>Welcome!!</h1>
+</body>
+</html>
