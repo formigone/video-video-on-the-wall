42ecf6d9c82d108874b64a9d4a6deae34680c395
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-04-30 12:59:53 -0600

    Updated donation text

--- application/views/scripts/donate/index.php
+++ application/views/scripts/donate/index.php
@@ -1,17 +1,17 @@
 <div class="container">
    <div class="row centered mt grid">
-      <h3>SHOW YOUR LOVE!</h3>
+      <h3>SUPPORT FREE EDUCATION!</h3>
 
       <p>If you enjoy our free programming tutorials, and you have a dollar or two to spare, we would be eternally
          grateful for your support.</p>
 
-      <p>To be perfectly honest, we don't need the money -- we definitelly take the money, but we really don't need any
-         of it. Yes, producing these videos do take a lot of time and effort. No, the primary goal of these videos is
+      <p>Even though making these videos is very satisfying and lots of fun, producing each videos do take a lot of time
+         and effort. As stated before, the primary goal of these videos is
          not to make us money. The biggest and ultimate reason and purpose for the videos is simply to share knowlegde
          about software development, help others become better programmers, co-workers, and people, and to have fun.
          That's all there's to it.</p>
 
-      <p>With that said, we do thank you for any monetary support.</p>
+      <p>With that said, we do thank you for your support though liking our videos, subscribing, and other donations.</p>
    </div>
 
    <div class="row centered">
@@ -31,24 +31,21 @@
          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="display: inline-block">
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="46ZNA9Q39EQGS">
-            <input type="submit" class="btn btn-theme"
-                   name="submit" value="$5">
+            <input type="submit" class="btn btn-theme" name="submit" value="$5">
             <img border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
          </form>
 
          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="display: inline-block">
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="WNZ8Y5L7WQGV4">
-            <input type="submit" class="btn btn-theme"
-                   name="submit" value="$25">
+            <input type="submit" class="btn btn-theme" name="submit" value="$25">
             <img border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
          </form>
 
          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="display: inline-block">
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="V66LNQJP9RZCA">
-            <input type="submit" class="btn btn-success"
-                   name="submit" value="$99">
+            <input type="submit" class="btn btn-success" name="submit" value="$99">
             <img border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
          </form>
       </div>
