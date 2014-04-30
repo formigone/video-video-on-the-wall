b69c98aa5d6bb21c8e876a4acededf7b1ab4178b
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-04-30 17:41:20 -0600

    Copied output class based off system/output

--- application/core/Fmg_Output.php
+++ application/core/Fmg_Output.php
@@ -1,34 +1,22 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-/**
- * @author vee
- * @copyright http://www.okvee.net
- */
-
 class Fmg_Output extends CI_Output {
    function __construct() {
       parent::__construct();
    }
 
    /**
-    * Update/serve a cached file
     *
-    * @access    public
-    * @return    void
     */
    function _display_cache(&$CFG, &$URI) {
       $cache_path = ($CFG->item('cache_path') == '') ? APPPATH . 'cache/' : $CFG->item('cache_path');
 
+      // Build the file path.  The file greet is an MD5 hash of the full URI
       $uri = $CFG->item('base_url') .
          $CFG->item('index_page') .
-         $URI->uri_string;
+         $URI->uri_string .
+         $_SERVER['QUERY_STRING'];
 
-      $querystrings = $_SERVER['QUERY_STRING'];
-      if ($querystrings != null) {
-         $querystrings = "?" . $querystrings;
-      }
-
-      $uri = $uri . $querystrings;
       $filepath = $cache_path . md5($uri);
 
       if (!@file_exists($filepath)) {
@@ -42,7 +30,6 @@ class Fmg_Output extends CI_Output {
       flock($fp, LOCK_SH);
 
       $cache = '';
-
       if (filesize($filepath) > 0) {
          $cache = fread($fp, filesize($filepath));
       }
@@ -50,64 +37,62 @@ class Fmg_Output extends CI_Output {
       flock($fp, LOCK_UN);
       fclose($fp);
 
+      // Strip out the embedded timestamp
       if (!preg_match("/(\d+TS--->)/", $cache, $match)) {
          return FALSE;
       }
 
+      // Has the file expired? If so we'll delete it.
       if (time() >= trim(str_replace('TS--->', '', $match['1']))) {
          if (is_really_writable($cache_path)) {
             @unlink($filepath);
-
             return FALSE;
          }
       }
 
+      // Display the cache
       $this->_display(str_replace($match['0'], '', $cache));
 
       return TRUE;
    }
 
    /**
-    * Write a Cache File
     *
-    * @access    public
-    * @return    void
     */
    function _write_cache($output) {
       $CI =& get_instance();
       $path = $CI->config->item('cache_path');
-      $cache_path = ($path == '') ? APPPATH . 'cache/' : $path;
 
-      if (!is_dir($cache_path) OR !is_really_writable($cache_path)) {
+      $cache_path = ($path == '') ? APPPATH.'cache/' : $path;
+
+      if ( ! is_dir($cache_path) OR ! is_really_writable($cache_path))
+      {
          return;
       }
 
-      $uri = $CI->config->item('base_url') .
-         $CI->config->item('index_page') .
-         $CI->uri->uri_string();
+      $uri =	$CI->config->item('base_url').
+         $CI->config->item('index_page').
+         $CI->uri->uri_string().
+         $_SERVER['QUERY_STRING'];
 
-      $querystrings = $_SERVER['QUERY_STRING'];
-
-      if ($querystrings != null) {
-         $querystrings = "?" . $querystrings;
-      }
-
-      $uri = $uri . $querystrings;
       $cache_path .= md5($uri);
 
-      if (!$fp = @fopen($cache_path, FOPEN_WRITE_CREATE_DESTRUCTIVE)) {
+      if ( ! $fp = @fopen($cache_path, FOPEN_WRITE_CREATE_DESTRUCTIVE))
+      {
          return;
       }
 
       $expire = time() + ($this->cache_expiration * 60);
 
-      if (flock($fp, LOCK_EX)) {
-         fwrite($fp, $expire . 'TS--->' . $output);
+      if (flock($fp, LOCK_EX))
+      {
+         fwrite($fp, $expire.'TS--->'.$output);
          flock($fp, LOCK_UN);
-      } else {
+      }
+      else
+      {
          return;
       }
-
       fclose($fp);
       @chmod($cache_path, FILE_WRITE_MODE);
    }
